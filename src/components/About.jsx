31ea470e477e0199246015e9e30bd51c7395d786
Tapfuma <tapfuma@MacBook-Pro.local>
2024-11-27 18:15:39 +0200

    initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        Hello and welcome to my website. I'm a dedicated web developer committed to delivering high-quality, innovative solutions. I'm proficient in HTML/CSS, PHP/MySQL, WordPress, JavaScript, jQuery and React. I also have working knowledge of Python and Ruby.  I enjoy collaborating with designers and other developers to bring creative projects to life."
+
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, 'about');
---
+++ src/constants/index.js
@@ -0,0 +1,271 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'UI/UX Design',
+    icon: ux,
+  },
+  {
+    title: 'Software Prototyping',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'Rails',
+    icon: rubyrails,
+  },
+  {
+    name: 'graphql',
+    icon: graphql,
+  },
+  {
+    name: 'postgresql',
+    icon: postgresql,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: 'IT Technician',
+    company_name: 'Zimbabwe School Examination Council',
+    icon: coverhunt,
+    iconBg: '#333333',
+    date: 'October 2017 - Jan 2018',
+  },
+  {
+    title: 'Web Designer',
+    company_name: 'Freelance',
+    icon: microverse,
+    iconBg: '#333333',
+    date: 'Feb 2018 - Mar 2020',
+  },
+  {
+    title: 'Web Developer',
+    company_name: 'Appointment Corporate Zimbabwe',
+    icon: kelhel,
+    iconBg: '#333333',
+    date: 'Mar 2020 - Oct 2023',
+  },
+  {
+    title: 'Full Stack Developer',
+    company_name: 'Freelance',
+    icon: dcc,
+    iconBg: '#333333',
+    date: 'Nov 2023 - Present',
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'Appointment',
+    description: 'A corporate wear and PPE company website',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: komikult,
+    repo: '#',
+    demo: 'https://appointment.co.zw/',
+  },
+  {
+    id: 'project-2',
+    name: 'Cr8iveNest',
+    description:
+      'Website for Cr8iveNest Entertainment, a UK based music label',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: leaderboard,
+    repo: '#',
+    demo: 'https://cr8ivenest.co.uk',
+  },
+  {
+    id: 'project-3',
+    name: 'Tiles And Styles',
+    description: 'This is a website for floor and wall tiles supplier.',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: math,
+    repo: '#',
+    demo: 'https://tilesandstyles.co.zw',
+  },
+  {
+    id: 'project-4',
+    name: 'Aurora Graphics',
+    description: 'Website for a Aurora Graphics, a leading marketing agency in Harare',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: movie,
+    repo: '#',
+    demo: 'https://aurora.co.zw',
+  },
+  {
+    id: 'project-5',
+    name: 'JA Consultancy',
+    description:
+      'This is a website for an accounting consultancy firm',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: nyeusi,
+    repo: '#',
+    demo: 'https://jaconsultancy.co.zw',
+  },
+];
+
+export { services, technologies, experiences, projects };
